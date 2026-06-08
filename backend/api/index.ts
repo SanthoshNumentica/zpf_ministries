6e7754c3a4091fe8ef64b16d0d47fe757f3c2ef7
Santhosh <sadaiyappan.s@numentica-ui.com>
2026-06-08 12:45:15 +0530

    app module

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import express from 'express';
-import { AppModule } from '../dist/app.module';
+import { AppModule } from '../src/app.module';
 
 const server = express();
 
